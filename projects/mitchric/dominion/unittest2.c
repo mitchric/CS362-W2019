734e0276801ed47416d83b9916e011fe3707c85c
mitchric <mitchric@oregonstate.edu>
2019-02-07 15:00:50 -0500

    Add unit test 2

---
+++ projects/mitchric/dominion/unittest2.c
@@ -0,0 +1,31 @@
+#include "dominion.h"
+#include <stdio.h>
+#include "rngs.h"
+#include <stdlib.h>
+
+void testWhoseTurn()
+{
+    int randomSeed = 100;
+    struct gameState G;
+    int k[10] = {adventurer, gardens, embargo, village, minion, mine, cutpurse,
+                 sea_hag, tribute, smithy};
+
+    for (int i = 2; i <= MAX_PLAYERS; ++i) {
+        initializeGame(i, k, randomSeed, &G);
+        printf("Testing if whoseTurn() reports correct turn index (zero based) for %d players.\n", i);
+        for (int j = 0; j < i; ++j) {
+            if (whoseTurn(&G) == j)
+                printf("whoseTurn(): PASS if turn equals player %d\n", j);
+            else 
+                printf("whoseTurn(): FAIL if turn does not equal %d\n", j);
+            endTurn(&G);
+        }
+    }
+}
+
+int main(int argc, char **argv)
+{
+    testWhoseTurn();
+
+    return 0;
+}
