d66761d3b7565e1670c3632ae6cd47c38085bbfd
mitchric <mitchric@oregonstate.edu>
2019-03-15 13:31:26 -0400

    Add URLIncorrect changes

--- FinalProject/URLValidatorInCorrect/test/UrlValidatorTest.java
+++ FinalProject/URLValidatorInCorrect/test/UrlValidatorTest.java
@@ -1,3 +1,6 @@
+// Richard Mitchell - mitchric
+// Tucker Lavell - lavellt
+// Matthew McLean - mcleanm
 
 
 import junit.framework.TestCase;
@@ -7,7 +10,7 @@ import junit.framework.TestCase;
 // Again, it is up to you to use this file or not!
 
 
-
+// added new branch
 
 
 public class UrlValidatorTest extends TestCase {
@@ -43,7 +46,92 @@ public class UrlValidatorTest extends TestCase {
 	   //You can use this function for programming based testing
 
    }
+
+   public void testIsValidUnitTest() {
+	  UrlValidator urlVal = new UrlValidator();
+
+	  System.out.println("Unit testing url http://www.apache.org"); 
+	  System.out.println("Expecting true");
+	  System.out.println("Result = " + urlVal.isValid("http://www.apache.org"));
+
+	  System.out.println("\nUnit testing url ftp://billgates:moremoney@files.microsoft.com/special"); 
+	  System.out.println("Expecting true");
+	  System.out.println("Result = " + urlVal.isValid("ftp://billgates:moremoney@files.microsoft.com/special"));
+	  
+	  
+	  System.out.println("\nUnit testing url 3ht://yahoo.com"); 
+	  System.out.println("Expecting false");
+	  System.out.println("Result = " + urlVal.isValid("3ht://yahoo.com"));
+	  
+	  System.out.println("\nUnit testing url http:/bing.com"); 
+	  System.out.println("Expecting false");
+	  System.out.println("Result = " + urlVal.isValid("http:/bing.com"));
+	  
+	  
+	  System.out.println("\nUnit testing url http:marvel.com"); 
+	  System.out.println("Expecting false");
+	  System.out.println("Result = " + urlVal.isValid("http:marvel.com"));
+	  
+	  System.out.println("\nUnit testing url http/starwars.com"); 
+	  System.out.println("Expecting false");
+	  System.out.println("Result = " + urlVal.isValid("http/starwars.com")); 
+	  
+	  System.out.println("\nUnit testing url ://nfl.com"); 
+	  System.out.println("Expecting false");
+	  System.out.println("Result = " + urlVal.isValid("://nfl.com"));
+   }
    
+   public void testIsValidSchemeUnitTest() {
+	  UrlValidator urlVal = new UrlValidator();
 
+	  System.out.println("\nUnit testing scheme http://"); 
+	  System.out.println("Expecting true");
+	  System.out.println("Result = " + urlVal.isValidScheme("http://"));
 
+	  System.out.println("\nUnit testing scheme ftp://"); 
+	  System.out.println("Expecting true");
+	  System.out.println("Result = " + urlVal.isValidScheme("ftp://"));
+	  
+	  
+	  System.out.println("\nUnit testing scheme 3ht://"); 
+	  System.out.println("Expecting false");
+	  System.out.println("Result = " + urlVal.isValidScheme("3ht://"));
+	  
+	  System.out.println("\nUnit testing scheme http:/"); 
+	  System.out.println("Expecting false");
+	  System.out.println("Result = " + urlVal.isValidScheme("http:/"));
+	  
+	  
+	  System.out.println("\nUnit testing scheme http:"); 
+	  System.out.println("Expecting false");
+	  System.out.println("Result = " + urlVal.isValidScheme("http:"));
+	  
+	  System.out.println("\nUnit testing scheme http/"); 
+	  System.out.println("Expecting false");
+	  System.out.println("Result = " + urlVal.isValidScheme("http/")); 
+	  
+	  System.out.println("\nUnit testing scheme ://"); 
+	  System.out.println("Expecting false");
+	  System.out.println("Result = " + urlVal.isValidScheme("://"));
+   } 
+   
+   public void testIsValidQueryUnitTest() {
+	  UrlValidator urlVal = new UrlValidator();
+	  
+	  System.out.println("\nUnit testing query ?hpt=sitenav");
+	  System.out.println("Expecting true");
+	  System.out.println("Result = " + urlVal.isValidQuery("?hpt=sitenav"));
+	  
+	  System.out.println("\nUnit testing query ?action=view");
+	  System.out.println("Expecting true");
+	  System.out.println("Result = " + urlVal.isValidQuery("?action=view"));
+	  
+	  System.out.println("\nUnit testing query ?action=edit&mode=up");
+	  System.out.println("Expecting true");
+	  System.out.println("Result = " + urlVal.isValidQuery("?action=edit&mode=up"));
+	  
+	  System.out.println("\nUnit testing empty query");
+	  System.out.println("Expecting true");
+	  System.out.println("Result = " + urlVal.isValidQuery(""));
+   }
 }
